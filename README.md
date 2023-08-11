a154db0da0ee0a28714b414ebd28c4508cc2f6af
Alex Parker <parkeralexjm@gmail.com>
2023-08-11 12:32:30 +0100

    Added folder structure ready for deployment"

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# puckman-clone
+# General Assembly
+
+## Link to the game
+
+## Overview and Concept
+
+Pac Man is a classic arcade game from the 80s. The player aims to eat all the food in a maze whilst being hunted by ghosts.
+
+If the player eats special flashing food the ghosts start to flash and can now be captured by the player, sending them back to their holding pen, from where they can once again start to hunt the player.
+
+The aim is to achieve the highest score possible before being killed by the ghosts.
+
+## Technologies used
+
+CSS reset from - [CSS reset - Andy Bell](https://andy-bell.co.uk/a-modern-css-reset/)
+
+### HTML
+
+### CSS
+
+### Javascript
+
+## Approach taken
---
+++ js/app.js
@@ -0,0 +1,72 @@
+// ! Elements
+
+// header > player & highscore
+
+// main wrapper
+
+// footer > lives & bonus icons
+
+// ! Variables
+
+// Maze size and empty array
+
+// Game speed (feed this in as the variable in intervals)
+
+// Starting positions as a cell number
+// wall array
+
+// Zones that are walls to prevent movement
+
+// ! Page Load
+
+// Load the splash page
+
+// ! Executions
+// Grid generation
+
+// Timeout for ghosts staying inside the center
+
+// Functions for keypress movement (use keydown?)
+// set interval
+
+// Logic for 'random' ghost movement
+
+// Logic for collision between:
+// Pacman and pellet
+// Pacman and power pellet - change ghost behaviour
+// Pacman and walls
+// Ghosts and walls
+// Pacman and bonuses
+// Pacman and Ghosts (scared) gain points and move ghost back to start
+// Pacman and Ghosts (chase) lose life
+
+// Eat all the pellets => generate a new screen with a pop up.
+
+// Adding, removing and moving classes to grid items
+
+// Game over when lost 3 lives
+
+// ! Events
+
+// start game on splash page
+
+// keypress
+
+// * ----- STRETCH CONTENT -----
+
+// Ghosts to have complex movement based on their name/'personality'
+
+// Game to store the high score in the local machine
+
+// Boards get more difficult:
+// Game speeds up each time a screen is cleared
+// Ghosts spend less time in 'scared' mode after power pellet
+
+// Research timings for fruit appearing and score etc.
+
+// Blinky - always takes the shortest path to pacman
+// Pinky - tries to get to the point 4 spaces in front of pacman
+// Inky
+// Clyde - True random movement
+
+// ? Multiple options to change the maze shape (ms Pacman)
