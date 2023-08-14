126ee5f3669fc5ba39e3a4a9f631d6f2665443f2
Alex Parker <parkeralexjm@gmail.com>
2023-08-14 09:11:39 +0100

    Added maze structure, display of dots and image files

--- js/app.js
+++ js/app.js
@@ -1,28 +1,143 @@
 // ! Elements
 
 // header > player & highscore
+const playerNameDisplay = document.getElementById("player-name");
+const currentScoreDisplay = document.getElementById("current-score-display");
+const highScoreDisplay = document.getElementById("high-score-display");
+
+// Splash page
+const splashDisplay = document.getElementById("splash");
 
 // main wrapper
+const gridWrapper = document.getElementById("grid-wrapper");
 
 // footer > lives & bonus icons
+const livesDisplay = document.getElementById("lives");
+const bonusDisplay = document.getElementById("bonus");
 
 // ! Variables
 
 // Maze size and empty array
+const width = 28;
+const height = 31;
+const gridSize = width * height;
+const gridReference = [];
+// Go through each tile of the maze and assign it a value that can be referenced later
+const mazeLayout = [
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1,
+  1, 2, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 2, 1,
+  1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1,
+  1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1,
+  1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1,
+  1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+  1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+  1, 1, 1, 1, 1, 1, 0, 1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+  1, 1, 1, 1, 1, 1, 0, 1, 1, 4, 1, 1, 1, 3, 3, 1, 1, 1, 4, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+  1, 1, 1, 1, 1, 1, 0, 1, 1, 4, 1, 3, 3, 3, 3, 3, 3, 1, 4, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+  4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 1, 3, 3, 3, 3, 3, 3, 1, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4,
+  1, 1, 1, 1, 1, 1, 0, 1, 1, 4, 1, 3, 3, 3, 3, 3, 3, 1, 4, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+  1, 1, 1, 1, 1, 1, 0, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 4, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+  1, 1, 1, 1, 1, 1, 0, 1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+  1, 1, 1, 1, 1, 1, 0, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 4, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+  1, 1, 1, 1, 1, 1, 0, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 4, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1,
+  1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1,
+  1, 2, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 2, 1,
+  1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1,
+  1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1,
+  1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1,
+  1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1,
+  1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+];
+// pellet = 0
+// wall = 1
+// power pellet = 2
+// ghost pen = 3
+// blank = 4
+
+let currentScore = 0;
+let highScore = 0;
+let lives = 3;
+let ghosts = ["binky", "inky", "pinky", "clyde"];
+// ghosts.map((ghost) => {
+//   let ghostAttributes = {}
+//   ghostAttributes.name = ghost
+//   ghostAttributes.upImage
+//   ghostAttributes.downImage
+//   ghostAttributes.leftImage
+//   ghostAttributes.rightImage
+// })
+
+// let bonus = [
+//   { name: "cherry", value: 100 },
+//   { name: "strawberry", value: 300 },
+//   { name: "orange", value: 500 },
+//   { name: "apple", value: 700 },
+//   { name: "melon", value: 1000 },
+//   { name: "galaxian", value: 2000 },
+//   { name: "bell", value: 3000 },
+//   { name: "key", value: 5000 },
+// ];
 
 // Game speed (feed this in as the variable in intervals)
-
-// Starting positions as a cell number
-// wall array
-
-// Zones that are walls to prevent movement
 
 // ! Page Load
 
 // Load the splash page
+mazeGenerator();
+scoreUpdate();
+livesUpdate();
+bonusUpdate();
+
+playerNameDisplay.innerHTML = "1UP";
+currentScoreDisplay.innerHTML = currentScore;
+highScoreDisplay.innerHTML = highScore > currentScore ? highScore : currentScore; // Function this later
 
 // ! Executions
 // Grid generation
+function mazeGenerator() {
+  // Create a grid with the variable gridSize and display their number inside
+  for (i = 0; i < gridSize; i++) {
+    let gridItem = document.createElement("div");
+    // Give all the item a class of gridItem
+    gridItem.classList.add("gridItem");
+    // Display the grid number on the item
+
+    if (mazeLayout[i] === 0) {
+      gridItem.classList.add("pellet");
+    } else if (mazeLayout[i] === 2) {
+      gridItem.classList.add("power-pellet");
+    }
+    // Set the width to be a percentage of the width
+    gridItem.style.width = `${100 / width}%`;
+    // Set the width to be a percentage of the height
+    gridItem.style.height = `${100 / width}%`;
+    gridReference.push(gridItem);
+    gridWrapper.append(gridItem);
+  }
+}
+
+function scoreUpdate() {
+  currentScoreDisplay.innerHTML = currentScore;
+  highScoreDisplay.innerHTML = highScore > currentScore ? highScore : currentScore; // Function this later
+}
+
+function livesUpdate() {
+  lives--;
+  livesDisplay.innerHTML = lives;
+}
+
+function bonusUpdate() {
+  // bonus.push(fruit)
+  bonusDisplay.innerHTML = "cherry";
+}
 
 // Timeout for ghosts staying inside the center
 
